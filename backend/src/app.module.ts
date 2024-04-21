09cc023d06af55ff3f72bba376e8bad6003527c0
Yulia <lutsenkoyulia2004@gmail.com>
2024-04-22 02:24:45 +0300

    fixed db config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -12,12 +12,7 @@ import { DealsModule } from './deals.module';
   imports: [
     ConfigModule.forRoot({
       validationSchema: Joi.object({
-        POSTGRES_HOST: Joi.string().required(),
-        POSTGRES_PORT: Joi.number().required(),
-        POSTGRES_USER: Joi.string().required(),
-        POSTGRES_PASSWORD: Joi.string().required(),
-        POSTGRES_DB: Joi.string().required(),
-        PORT: Joi.number(),
+        DB_URL: Joi.string().required(),
       }),
     }),
     DatabaseModule,
